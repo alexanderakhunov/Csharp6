e63753fe12f31e30bfdfda926aae5be894d9f836
ALEXANDRO <sancheek240@mail.ru>
2023-02-05 14:03:33 +0300

    Add Ex1-2

---
+++ Ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArr(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i} элемент: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int CountArr(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    System.Console.WriteLine($"Кол-во элементов > 0 = {count}");
+    return count;
+}
+
+int[] array = FillArr(5);
+PrintArr(array);
+CountArr(array);
